6bfff24670585d39399336ceb8223116e1558959
Pedro V. <joao-ventura55@hotmail.com>
2024-11-18 07:24:38 -0300

    Add project files.

---
+++ BugReportTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BugReportTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
